0aa3d919f05581fcf70617c1a4eab1ed416378e3
Илья Олейников <oleynikovis@yandex.ru>
2024-06-27 15:26:34 +0300

    test proxy

--- run.py
+++ run.py
@@ -4,10 +4,9 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.client.session.aiohttp import AiohttpSession
 
-from app.handlers import router
-
-from config import TOKEN
 from app.database.models import async_main
+from app.handlers import router
+from config import TOKEN
 
 proxy = 'http://161.34.39.156:3128'
 session = AiohttpSession(proxy=proxy)
@@ -25,8 +24,15 @@ async def on_startup(dispatcher):
     await async_main()
 
 
+async def test_proxy(proxy):
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://api.ipify.org?format=json', proxy=proxy) as resp:
+            print(await resp.json())
+
+
 if __name__ == '__main__':
     try:
         asyncio.run(main())
+        asyncio.run(test_proxy(proxy))
     except KeyboardInterrupt:
         pass
